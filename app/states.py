6daed2a7056e8b09f32a28199eb5d0b57231828f
DEMOHCHIK <smnvmaks@gmail.com>
2024-05-19 11:12:40 +0600

    Добавил состояние Партнёра, для поиска по username

--- app/states.py
+++ app/states.py
@@ -1,6 +1,10 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
-class NewGift(StatesGroup):
+class Gift(StatesGroup):
     name = State()
     description = State()
+
+
+class Partner(StatesGroup):
+    username = State()
